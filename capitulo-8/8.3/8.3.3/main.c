aa0f6169d0616a674fd52864c779b2baf75e9f88
Marcos Hernandez <marcos.hernandez.dev@gmail.com>
2023-07-03 18:46:18 -0300

    calcular la cantidad de registros que tiene un archivo

---
+++ capitulo-8/8.3/8.3.3/main.c
@@ -0,0 +1,25 @@
+// Agregar un registro al final del archivo
+
+#include <stdio.h>
+#include "alumno.h"
+
+int main()
+{
+	FILE* archivo = fopen("alumnos.dat", "r+b");
+
+	// Ingreso los datos por consola
+	Alumno alumno = ingresoDatosXConsola();
+
+	// Posiciono el puntero al final del archivo
+	fseek(archivo, 0, SEEK_END);
+
+	// Grabo el nuevo registro
+	fwrite(&alumno, sizeof(Alumno), 1, archivo);
+
+	long cantReg = ftell(archivo)/sizeof(Alumno);
+	printf("\nEl archivo tiene %ld registros\n", cantReg);
+
+	fclose(archivo);
+
+	return 0;
+}
