ea73eb6d8e08cf2f6a5fe587fb3b4c59333dbae5
Marcos Hernandez <marcos.hernandez.dev@gmail.com>
2023-07-04 00:30:32 -0300

    limitaciones de los archivos secuenciales

---
+++ capitulo-8/8.6/8.6.1/empleado.h
@@ -0,0 +1,14 @@
+// Limitaciones de los archivos secuenciales
+
+#ifndef EMPLEADO_H
+#define EMPLEADO_H
+
+typedef struct Empleado
+{
+	int idEmpleado;
+	char nombre[30];
+	char direccion[120];
+	long fechaIngreso;
+} Emp;
+
+#endif
