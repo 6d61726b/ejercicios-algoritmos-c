a8b1f4d2c54c72f48a50feb1d15824b07f66c84f
Marcos Hernandez <marcos.hernandez.dev@gmail.com>
2023-07-05 19:21:47 -0300

    feat: typedef IndexIdEmpleado

---
+++ capitulo-8/8.6/8.6.6/empleado.h
@@ -0,0 +1,26 @@
+// Indexacion de archivos
+
+#ifndef EMPLEADO_H
+#define EMPLEADO_H
+
+#include <stdlib.h>
+
+typedef struct IndexIdEmpleado
+{
+	int idEmpleado;
+	int posicion;
+} IndexIdEmpleado;
+
+typedef struct Empleado
+{
+	int idEmpleado;
+	char nombre[30];
+	char direccion[120];
+	long fechaIngreso;
+} Empleado;
+
+int subirArchivo(FILE *archivo, Empleado empleados[]);
+void ordenar(Empleado empleados[], int longitud);
+void bajarArchivo(FILE *archivo, Empleado empleados[], int longitud);
+
+#endif
